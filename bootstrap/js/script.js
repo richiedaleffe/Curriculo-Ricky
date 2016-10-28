ed1519e8d944da15afc1a3140f7e55287624b582
richiedaleffe <richie_daleffe@hotmail.com>
2016-10-28 20:01:32 -0200

    upload1

---
+++ bootstrap/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+
+    function enviarEmail() {
+
+        var $requisicao = $.ajax({
+            method: "POST",
+            url: 'http://contato-twsatc.rhcloud.com/contato-api/email',
+            crossDomain: true,
+            data: {
+                nome: $("#nome").val(),
+                email: $("#email").val(),
+                telefone: $("#telefone").val(),
+                mensagem: $("#mensagem").val(),
+                para: $("#para").val()
+            }
+        });
+
+        $requisicao.then(function (resposta) {
+
+            $("#carregando").hide();
+            if (resposta === true) {
+                $("#contact-form").trigger("reset");
+                $("#mensagem-sucesso").addClass("show");
+            } else {
+                $("#mensagem-erro").addClass("show");
+            }
+        });
+    }
+
+
+    $("#contact-form").on("submit", function (event) {
+        event.preventDefault();
+        enviarEmail();
+    });
+
+    $(document).ajaxStart(function () {
+        $("#carregando").show();
+    });
+       
+
+});
